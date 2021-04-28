7e8d0554f53e1c9a280bd4c0a3a3ade53dc21393
sebamacchia <jsmacchia@yahoo.es>
2021-04-28 12:21:04 -0300

    c

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # app.py
 from flask import Flask, request, jsonify
 
-import sys
+# import sys
 import os
-import glob
-import re
+# import glob
+# import re
 import numpy as np
 import tensorflow as tf
 import pandas as pd
@@ -18,56 +18,13 @@ from tensorflow import keras
 
 # Flask utils
 # from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+# from werkzeug.utils import secure_filename
+# from gevent.pywsgi import WSGIServer
 
 from PIL import Image, ImageChops
 
 
 app = Flask(__name__)
-
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
 
 
 @app.route('/')
