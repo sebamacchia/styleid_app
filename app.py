baf3154756fec042c31d4b0bdbb106329c016f95
sebamacchia <jsmacchia@yahoo.es>
2021-04-28 12:15:59 -0300

    c

--- app.py
+++ app.py
@@ -1,8 +1,72 @@
 # app.py
+from PIL import Image, ImageChops
+from gevent.pywsgi import WSGIServer
+from werkzeug.utils import secure_filename
+from tensorflow import keras
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import re
+import glob
+import os
 from flask import Flask, request, jsonify
+
+mport sys
+
+
+# Keras
+
+# Flask utils
+# from flask import Flask, redirect, url_for, request, render_template
 
 
 app = Flask(__name__)
+
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD": "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
 
 
 @app.route('/')
