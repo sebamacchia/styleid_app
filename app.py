8c5a980081358ead736c7d93f1e07e3eb657af8b
sebamacchia <jsmacchia@yahoo.es>
2021-04-28 12:07:32 -0300

    c

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
 # app.py
-# from flask import Flask, request, jsonify
-
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
-import tensorflow as tf
-import pandas as pd
+from flask import Flask, request, jsonify
 
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-from tensorflow import keras
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template, jsonify
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-from PIL import Image, ImageChops
-
-# Define a flask app
 app = Flask(__name__)
+
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD": "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+
 
 @app.route('/')
 def index():
-    return "<h1>STYLEID !!</h1>"
+    return "<h1>HOLA STYLEID !!</h1>"
 
 
 if __name__ == '__main__':
