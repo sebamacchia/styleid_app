5fbac65cc5a2f5e842725867ec5e9086531c9a64
sebamacchia <jsmacchia@yahoo.es>
2021-04-29 17:03:13 -0300

    c

--- app.py
+++ app.py
@@ -42,10 +42,10 @@ def index():
     # data = pd.read_csv(
     #     'https://styleidam.s3-us-west-1.amazonaws.com/prueba.csv')
     # model = tf.keras.models.load_model(
-        'https://styleidam.s3-us-west-1.amazonaws.com/model_baseline01')
+    # 'https://styleidam.s3-us-west-1.amazonaws.com/model_baseline01')
     return "<h1>m: {interpreter}</h1>"
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded = True, port = 5000)
+    app.run(threaded=True, port=5000)
